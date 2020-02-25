020f9fb824ebf248be4853f38d84e4fb683c3fdf
Justine Duhieu <justine.duhieu@epitech.eu>
2020-02-25 15:28:34 +0100

    Add Facebook get the post I like route

---
+++ back/Models/FacebookApi/FacebookDataLikeModel.cs
@@ -0,0 +1,12 @@
+﻿﻿using System.Collections.Generic;
+ using DashboardAPI.Models.FbModels;
+ using Newtonsoft.Json;
+
+ namespace back.Models.FacebookApi
+{
+    public class FacebookDataLike
+    {
+        [JsonProperty("data")]
+        public List<FacebookLikeStringModel> Data { get; set; }
+    }
+}
