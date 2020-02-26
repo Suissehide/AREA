ca934157c86c3fd9bb3d407c4a10041293c0a627
!doia <idoia.reinares@epitech.eu>
2020-02-27 00:35:50 +0100

    [FIX] Fix broken TextInput

--- mobile_application/widgets/MovieWidget.js
+++ mobile_application/widgets/MovieWidget.js
@@ -2,21 +2,23 @@ import React, { useState } from "react";
 import { View } from 'react-native';
 import axios from 'axios';
 import Text from '../components/Text'
-import { TextInput } from "react-native-paper";
+import TextInput from '../components/TextInput';
 
 export default function MovieWidget(props) {
     const [movie, setMovie] = useState('Harry Potter');
     const [title, setTitle] = useState('');
     const [overview, setOverview] = useState('');
 
-    axios.get(`http://${props.ip}:8080/api/TmdbApi/movie/${movie}`)
-        .then(response => {
-            setTitle(response.data[0].title);
-            setOverview(response.data[0].overview);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+    const handleChange = () => {
+        axios.get(`http://${props.ip}:8080/api/TmdbApi/movie/${movie}`)
+            .then(response => {
+                setTitle(response.data[0].title);
+                setOverview(response.data[0].overview);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 
     return (
         <View style={{ alignItems: 'center' }}>
@@ -30,9 +32,10 @@ export default function MovieWidget(props) {
                 id="outlined-name"
                 label="Movie Title"
                 value={movie}
-                onChange={event => {
-                    setMovie(event.target.value);
+                onChangeText={event => {
+                    setMovie(event);
                 }}
+                onSubmitEditing={handleChange}
                 margin="normal"
                 variant="outlined"
             />
