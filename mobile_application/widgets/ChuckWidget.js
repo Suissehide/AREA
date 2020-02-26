d1c2efcff914c76f4576a56598d3501a28a01df0
!doia <idoia.reinares@epitech.eu>
2020-02-26 22:50:41 +0100

    [FIX] Adding an OK Bottom to prevent auto refreshing. Also added divider + padding for the look

--- mobile_application/widgets/ChuckWidget.js
+++ mobile_application/widgets/ChuckWidget.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { StyleSheet, View, FormData } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Button from '../components/Button'
 import axios from 'axios';
 import Text from '../components/Text'
-import { TextInput } from "react-native-paper";
+import { TextInput, Divider } from "react-native-paper";
 
 export default function ChuckWidget(props) {
     const [chuck1, setChuck1] = useState("");
@@ -11,22 +11,30 @@ export default function ChuckWidget(props) {
     const [chuck3, setChuck3] = useState("");
     const [theme, setTheme] = useState("beer");
 
-    axios.get(`http://${props.ip}:8080/api/Chuck/${theme}`)
-        .then(response => {
-            if (response.data.total)
-                setChuck1(response.data.result[Math.floor(Math.random() * response.data.total)].value);
-            setChuck2(response.data.result[Math.floor(Math.random() * response.data.total)].value);
-            setChuck3(response.data.result[Math.floor(Math.random() * response.data.total)].value);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+    const handleChange = () => {
+        axios.get(`http://${props.ip}:8080/api/Chuck/${theme}`)
+            .then(response => {
+                if (response.data.total)
+                    setChuck1(response.data.result[Math.floor(Math.random() * response.data.total)].value);
+                setChuck2(response.data.result[Math.floor(Math.random() * response.data.total)].value);
+                setChuck3(response.data.result[Math.floor(Math.random() * response.data.total)].value);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 
     return (
         <View style={{ alignItems: 'center' }}>
             <View style={{ paddingTop: 10, paddingBottom: 20 }}>
                 <Text>{chuck1}</Text>
+                <View style={{ paddingBottom: 10, paddingTop: 10 }}>
+                    <Divider />
+                </View>
                 <Text>{chuck2}</Text>
+                <View style={{ paddingBottom: 10, paddingTop: 10 }}>
+                    <Divider />
+                </View>
                 <Text>{chuck3}</Text>
             </View>
             <TextInput
@@ -39,6 +47,7 @@ export default function ChuckWidget(props) {
                 margin="normal"
                 variant="outlined"
             />
+            <Button onPress={handleChange}>OK</Button>
         </View>
     );
 }
