90205a967f77fe23e9e549c5667f528490816bed
Justine Duhieu <justine.duhieu@epitech.eu>
2020-02-28 00:07:12 +0100

    Update name of variables jokeTheme

--- back/Clients/IJokeClient.cs
+++ back/Clients/IJokeClient.cs
@@ -7,7 +7,7 @@ namespace back.Clients
     public interface IJokeClient
     {
         [Headers("Accept: application/json")]
-        [Get("/search?accept=application/json&term={themeJoke}")]
-        Task<JokeModel> ApiGetJokeByTheme(string themeJoke);
+        [Get("/search?accept=application/json&term={jokeTheme}")]
+        Task<JokeModel> ApiGetJokeByTheme(string jokeTheme);
     }
 }
