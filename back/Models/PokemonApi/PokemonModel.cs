ddc78a80ce2709afa9e9e9720c8e2f4d454f9b2f
ThomasLesueur <35767490+ThomasLesueur@users.noreply.github.com>
2020-02-28 19:03:18 +0100

    modif pokemon model (PokemonModel.cs)

--- back/Models/PokemonApi/PokemonModel.cs
+++ back/Models/PokemonApi/PokemonModel.cs
@@ -10,41 +10,18 @@ namespace back.Models
         [JsonProperty("abilities")]
         public Ability[] Abilities { get; set; }
 
-        [JsonProperty("base_experience")]
-        public long BaseExperience { get; set; }
-
-        [JsonProperty("forms")]
-        public Species[] Forms { get; set; }
-
-        [JsonProperty("game_indices")]
-        public GameIndex[] GameIndices { get; set; }
 
         [JsonProperty("height")]
         public long Height { get; set; }
 
-        [JsonProperty("held_items")]
-        public HeldItem[] HeldItems { get; set; }
-
         [JsonProperty("id")]
         public long Id { get; set; }
-
-        [JsonProperty("is_default")]
-        public bool IsDefault { get; set; }
-
-        [JsonProperty("location_area_encounters")]
-        public Uri LocationAreaEncounters { get; set; }
-
-        [JsonProperty("moves")]
-        public Move[] Moves { get; set; }
 
         [JsonProperty("name")]
         public string Name { get; set; }
 
         [JsonProperty("order")]
         public long Order { get; set; }
-
-        [JsonProperty("species")]
-        public Species Species { get; set; }
 
         [JsonProperty("sprites")]
         public Sprites Sprites { get; set; }
