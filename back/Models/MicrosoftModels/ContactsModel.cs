0fb2608692dc9bdc6934e2611a52644b80bd454b
ThomasLesueur <35767490+ThomasLesueur@users.noreply.github.com>
2020-02-29 01:25:59 +0100

    Create ContactsModel.cs

---
+++ back/Models/MicrosoftModels/ContactsModel.cs
@@ -0,0 +1,24 @@
+using Newtonsoft.Json;
+
+namespace back.Models
+{
+    public class ContactsModel
+    {
+        [JsonProperty("value")]
+        public Value[] Value { get; set; }
+    }
+    public class Value
+    {
+        [JsonProperty("displayName")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("scoredEmailAddresses")]
+        public ScoredEmailAddress[] ScoredEmailAddresses { get; set; }
+    }
+
+    public class ScoredEmailAddress
+    {
+        [JsonProperty("address")]
+        public string Address { get; set; }
+    }
+}
