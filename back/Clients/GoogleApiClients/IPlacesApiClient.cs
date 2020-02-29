2c84879d85d969487b5f1b75bd804c7e863e6429
ThomaasLesueur <thomas.lesueur@epitech.eu>
2020-03-01 00:21:48 +0100

    modify get method for place detail

--- back/Clients/GoogleApiClients/IPlacesApiClient.cs
+++ back/Clients/GoogleApiClients/IPlacesApiClient.cs
@@ -10,7 +10,7 @@ namespace back.Clients.GoogleApi
         [Get("/maps/api/place/textsearch/json?query={place}&key={key}")]
         Task<PlacesApiModel> GetPlaces(string place, string key);
 
-        [Get("/maps/api/details/json?place_id={placeId}&fields=name,rating,formatted_phone_number&key={key}")]
+        [Get("/maps/api/place/details/json?place_id={placeId}&fields=name,rating,formatted_phone_number&key={key}")]
         Task<PlaceDetailApiModel> GetPlaceDetail(string placeId, string key);
     }
 
