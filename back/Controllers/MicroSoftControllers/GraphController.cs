cf45aa968907046a3d5921b8a9c0265b0cd1141b
ThomasLesueur <35767490+ThomasLesueur@users.noreply.github.com>
2020-02-29 16:23:39 +0100

    Create GraphController.cs

---
+++ back/Controllers/MicroSoftControllers/GraphController.cs
@@ -0,0 +1,34 @@
+using back.Clients;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Refit;
+using System.Threading.Tasks;
+
+namespace back.Controllers.MicrosoftControllers
+{
+    [ApiController]
+    [Route("api/microsoft/graph")]
+    public class GraphController : MicrosoftMainController
+    {
+        public GraphController(ILogger<MicrosoftMainController> logger) : base(logger)
+        {
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> ClientGetContactsAsync([Header("Authorization")] string authorization)
+        {
+            var contactApiClient = RestService.For<IGraphApi>(HostUrl);
+            try
+            {
+                var contacts = await contactApiClient.ApiGetGraph(authorization).ConfigureAwait(true);
+                return Ok(contacts);
+            }
+            catch (ApiException exMessage)
+            {
+                _logger.LogError(exMessage.Message);
+                return NotFound();
+            }
+        }
+    }
+}
