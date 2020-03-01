bb616cc555da55061f4bc0c44d100ddf968f4425
ThomaasLesueur <thomas.lesueur@epitech.eu>
2020-03-01 18:15:41 +0100

    [ADD] Controller for star wars

---
+++ back/Controllers/StarWarsApiController.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Threading.Tasks;
+using back.Clients.StarWarsApi;
+using back.Models.StarWarsApi.StarWarsPeopleModel;
+using back.Models.StarWarsApi.StarWarsPeoplesModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Refit;
+
+namespace back.Controllers
+{
+    [ApiController]
+    [Route("api/starwars")]
+    public class StarWarsApiController : ControllerBase
+    {
+        #region MEMBER
+     
+        private readonly ILogger<StarWarsApiController> _logger;
+        private readonly IStarWarsApiClient _starWarsCient;
+
+        #endregion
+        
+        #region CONSTRUCTOR
+
+        public StarWarsApiController(ILogger<StarWarsApiController> logger)
+        {
+            _logger = logger;
+            _starWarsCient = RestService.For<IStarWarsApiClient>("https://swapi.co/api/");
+        }
+
+        #endregion
+
+        #region ROUTE
+
+        [HttpGet("random")]
+        public async Task<IActionResult> HeroRandom()
+        {
+            try
+            {
+                Random rand = new Random();
+                int id = rand.Next(87);
+                StarWarsPeopleModel peopleInfo = await _starWarsCient.getCharacterById(id);
+                return Ok(peopleInfo);
+            }
+            catch (ApiException exMessage)
+            {
+                _logger.LogError(exMessage.Message);
+                return NotFound(exMessage);
+            }
+        }
+       
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllCharacters()
+        {
+            try
+            {
+                StarWarsPeoplesModel peoples = await _starWarsCient.GetAllCharacters();
+                return Ok(peoples);
+            }
+            catch (ApiException exMessage)
+            {
+                _logger.LogError(exMessage.Message);
+                return NotFound(exMessage);
+            }
+        }
+        
+        #endregion
+    }
+}
