a3179a7625d635e2ca60b21e3424be6f74fc1484
ThomaasLesueur <thomas.lesueur@epitech.eu>
2020-03-01 18:27:46 +0100

    [FIX] models

--- back/Models/StarWarsApi/StarWarsPeoplesModel.cs
+++ back/Models/StarWarsApi/StarWarsPeoplesModel.cs
@@ -27,12 +27,10 @@ namespace back.Models.StarWarsApi.StarWarsPeoplesModel
         public string Name { get; set; }
 
         [JsonProperty("height")]
-        [JsonConverter(typeof(ParseStringConverter))]
-        public long Height { get; set; }
+        public string Height { get; set; }
 
         [JsonProperty("mass")]
-        [JsonConverter(typeof(ParseStringConverter))]
-        public long Mass { get; set; }
+        public string Mass { get; set; }
 
         [JsonProperty("hair_color")]
         public string HairColor { get; set; }
@@ -76,104 +74,4 @@ namespace back.Models.StarWarsApi.StarWarsPeoplesModel
 
     public enum Gender { Female, Male, NA };
 
-    public partial class StarWarsPeoplesModel
-    {
-        public static StarWarsPeoplesModel FromJson(string json) => JsonConvert.DeserializeObject<StarWarsPeoplesModel>(json, back.Models.StarWarsApi.StarWarsPeoplesModel.Converter.Settings);
-    }
-
-    public static class Serialize
-    {
-        public static string ToJson(this StarWarsPeoplesModel self) => JsonConvert.SerializeObject(self, back.Models.StarWarsApi.StarWarsPeoplesModel.Converter.Settings);
-    }
-
-    internal static class Converter
-    {
-        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
-        {
-            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
-            DateParseHandling = DateParseHandling.None,
-            Converters =
-            {
-                GenderConverter.Singleton,
-                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
-            },
-        };
-    }
-
-    internal class GenderConverter : JsonConverter
-    {
-        public override bool CanConvert(Type t) => t == typeof(Gender) || t == typeof(Gender?);
-
-        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
-        {
-            if (reader.TokenType == JsonToken.Null) return null;
-            var value = serializer.Deserialize<string>(reader);
-            switch (value)
-            {
-                case "female":
-                    return Gender.Female;
-                case "male":
-                    return Gender.Male;
-                case "n/a":
-                    return Gender.NA;
-            }
-            throw new Exception("Cannot unmarshal type Gender");
-        }
-
-        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
-        {
-            if (untypedValue == null)
-            {
-                serializer.Serialize(writer, null);
-                return;
-            }
-            var value = (Gender)untypedValue;
-            switch (value)
-            {
-                case Gender.Female:
-                    serializer.Serialize(writer, "female");
-                    return;
-                case Gender.Male:
-                    serializer.Serialize(writer, "male");
-                    return;
-                case Gender.NA:
-                    serializer.Serialize(writer, "n/a");
-                    return;
-            }
-            throw new Exception("Cannot marshal type Gender");
-        }
-
-        public static readonly GenderConverter Singleton = new GenderConverter();
-    }
-
-    internal class ParseStringConverter : JsonConverter
-    {
-        public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
-
-        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
-        {
-            if (reader.TokenType == JsonToken.Null) return null;
-            var value = serializer.Deserialize<string>(reader);
-            long l;
-            if (Int64.TryParse(value, out l))
-            {
-                return l;
-            }
-            throw new Exception("Cannot unmarshal type long");
-        }
-
-        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
-        {
-            if (untypedValue == null)
-            {
-                serializer.Serialize(writer, null);
-                return;
-            }
-            var value = (long)untypedValue;
-            serializer.Serialize(writer, value.ToString());
-            return;
-        }
-
-        public static readonly ParseStringConverter Singleton = new ParseStringConverter();
-    }
 }
