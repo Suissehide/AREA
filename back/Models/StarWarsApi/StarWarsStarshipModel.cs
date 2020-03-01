f5dbb5c1af875a3ac5b79f740e7f104966925f1c
ThomaasLesueur <thomas.lesueur@epitech.eu>
2020-03-01 22:25:25 +0100

    add starship route

---
+++ back/Models/StarWarsApi/StarWarsStarshipModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace back.Models.StarWarsApi.StarWarsStarshipModel
+{
+    public partial class StarWarsStarshipModel
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("model")]
+        public string Model { get; set; }
+
+        [JsonProperty("manufacturer")]
+        public string Manufacturer { get; set; }
+
+        [JsonProperty("cost_in_credits")]
+        public string CostInCredits { get; set; }
+
+        [JsonProperty("length")]
+        public string Length { get; set; }
+
+        [JsonProperty("max_atmosphering_speed")]
+        public string MaxAtmospheringSpeed { get; set; }
+
+        [JsonProperty("crew")]
+        public string Crew { get; set; }
+
+        [JsonProperty("passengers")]
+        public string Passengers { get; set; }
+
+        [JsonProperty("cargo_capacity")]
+        public string CargoCapacity { get; set; }
+
+        [JsonProperty("consumables")]
+        public string Consumables { get; set; }
+
+        [JsonProperty("hyperdrive_rating")]
+        public string HyperdriveRating { get; set; }
+
+        [JsonProperty("MGLT")]
+        public string Mglt { get; set; }
+
+        [JsonProperty("starship_class")]
+        public string StarshipClass { get; set; }
+
+        [JsonProperty("pilots")]
+        public List<object> Pilots { get; set; }
+
+        [JsonProperty("films")]
+        public List<Uri> Films { get; set; }
+
+    }
+}
