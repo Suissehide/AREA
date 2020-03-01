c1d97d9315cf69149afc50f13408b55c2016bf3a
ThomaasLesueur <thomas.lesueur@epitech.eu>
2020-03-01 17:45:47 +0100

    [ADD] star wars model

---
+++ back/Models/StarWarsApi/StarWarsPeoplesModels.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace back.Models.StarWarsApi
+{
+    public partial class StarWarsPeoplesModel
+    {
+        [JsonProperty("count")]
+        public long Count { get; set; }
+
+        [JsonProperty("next")]
+        public Uri Next { get; set; }
+
+        [JsonProperty("previous")]
+        public object Previous { get; set; }
+
+        [JsonProperty("results")]
+        public List<Result> Results { get; set; }
+    }
+
+    public partial class Result
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("height")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long Height { get; set; }
+
+        [JsonProperty("mass")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long Mass { get; set; }
+
+        [JsonProperty("hair_color")]
+        public string HairColor { get; set; }
+
+        [JsonProperty("skin_color")]
+        public string SkinColor { get; set; }
+
+        [JsonProperty("eye_color")]
+        public string EyeColor { get; set; }
+
+        [JsonProperty("birth_year")]
+        public string BirthYear { get; set; }
+
+        [JsonProperty("gender")]
+        public Gender Gender { get; set; }
+
+        [JsonProperty("homeworld")]
+        public Uri Homeworld { get; set; }
+
+        [JsonProperty("films")]
+        public List<Uri> Films { get; set; }
+
+        [JsonProperty("species")]
+        public List<Uri> Species { get; set; }
+
+        [JsonProperty("vehicles")]
+        public List<Uri> Vehicles { get; set; }
+
+        [JsonProperty("starships")]
+        public List<Uri> Starships { get; set; }
+
+        [JsonProperty("created")]
+        public DateTimeOffset Created { get; set; }
+
+        [JsonProperty("edited")]
+        public DateTimeOffset Edited { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+    }
+
+    public enum Gender { Female, Male, NA };
+
+    public partial class StarWarsPeoplesModel
+    {
+        public static StarWarsPeoplesModel FromJson(string json) => JsonConvert.DeserializeObject<StarWarsPeoplesModel>(json, back.Models.StarWarsApi.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this StarWarsPeoplesModel self) => JsonConvert.SerializeObject(self, back.Models.StarWarsApi.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                GenderConverter.Singleton,
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+
+    internal class GenderConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(Gender) || t == typeof(Gender?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            switch (value)
+            {
+                case "female":
+                    return Gender.Female;
+                case "male":
+                    return Gender.Male;
+                case "n/a":
+                    return Gender.NA;
+            }
+            throw new Exception("Cannot unmarshal type Gender");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (Gender)untypedValue;
+            switch (value)
+            {
+                case Gender.Female:
+                    serializer.Serialize(writer, "female");
+                    return;
+                case Gender.Male:
+                    serializer.Serialize(writer, "male");
+                    return;
+                case Gender.NA:
+                    serializer.Serialize(writer, "n/a");
+                    return;
+            }
+            throw new Exception("Cannot marshal type Gender");
+        }
+
+        public static readonly GenderConverter Singleton = new GenderConverter();
+    }
+
+    internal class ParseStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            long l;
+            if (Int64.TryParse(value, out l))
+            {
+                return l;
+            }
+            throw new Exception("Cannot unmarshal type long");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (long)untypedValue;
+            serializer.Serialize(writer, value.ToString());
+            return;
+        }
+
+        public static readonly ParseStringConverter Singleton = new ParseStringConverter();
+    }
+}
