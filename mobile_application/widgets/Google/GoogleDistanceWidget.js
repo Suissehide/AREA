9ce3dc13dc90f28e13545955f93235edce98a73f
!doia <idoia.reinares@epitech.eu>
2020-03-02 21:34:00 +0100

    [MODIFY] Google distance widget is now taking modes !

--- mobile_application/widgets/Google/GoogleDistanceWidget.js
+++ mobile_application/widgets/Google/GoogleDistanceWidget.js
@@ -14,8 +14,8 @@ export default function GoogleDistanceWidget(props) {
     const [originAddresses, setOriginAddresses] = useState('');
     const [distance, setDistance] = useState('');
     const [duration, setDuration] = useState('');
-    const [status, setStatus] = useState('');
-    const [mode, setMode] = useState('');
+    const [status, setStatus] = useState(null);
+    const [mode, setMode] = useState(null);
     var key = "AIzaSyCQCPjXryFKgZ9wN9B5E6b05XgYH8yU8j0";
 
     function MyIcon(props) {
@@ -28,44 +28,46 @@ export default function GoogleDistanceWidget(props) {
         );
     }
 
+    const handleChange = () => { };
+
     useEffect(() => {
-        axios.get(`http://${props.ip}/api/google/maps/distance/origin=${origin}&destination=${destination}&mode=${mode}&key=${key}`)
-            .then(response => {
-                console.log(response.data)
-                setStatus(response.data.rows[0].elements[0].status);
-                setDestinationAddresses(response.data.destinationAddresses[0]);
-                setOriginAddresses(response.data.originAddresses[0]);
-                setDistance(response.data.rows[0].elements[0].distance);
-                setDuration(response.data.rows[0].elements[0].duration);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+        mode !== null ?
+            axios.get(`http://${props.ip}/api/google/maps/distance/mode/origin=${origin}&destination=${destination}&mode=${mode}&key=${key}`)
+                .then(response => {
+                    setStatus(response.data.rows[0].elements[0].status);
+                    setDestinationAddresses(response.data.destinationAddresses[0]);
+                    setOriginAddresses(response.data.originAddresses[0]);
+                    setDistance(response.data.rows[0].elements[0].distance);
+                    setDuration(response.data.rows[0].elements[0].duration);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                }) : null
     }, [mode]);
 
     return (
         <View style={{ alignItems: 'center' }}>
             <TextInput id="origin" label="From"
-                value={origin} onChangeText={event => { setOrigin(event); }}
+                value={origin} onChangeText={event => { setOrigin(event); setMode(null) }}
                 margin="normal" variant="outlined"
             />
             <TextInput id="destination" label="To"
                 value={destination}
-                onChangeText={event => { setDestination(event); }}
+                onChangeText={event => { setDestination(event); setMode(null) }}
                 margin="normal" variant="outlined"
             />
-            {status === "NOT_FOUND" || status === "ZERO_RESULTS" ? <Text>No results found.</Text> : status === "OK" ?
+            {status === "OK" ?
                 <Text swag={{ fontSize: 20 }} >
                     The distance between <Text swag={{ fontWeight: 'bold' }} >{originAddresses}</Text> and <Text swag={{ fontWeight: 'bold' }} >
                         {destinationAddresses}</Text> is <Text swag={{ fontWeight: 'bold' }} >{distance.text}</Text>.
                     {"\n"}It takes <Text swag={{ fontWeight: 'bold' }} >{duration.text}</Text> by {mode}.
-                </Text> : null
+                </Text> : status === null ? null : <Text> No results found. </Text>
             }
             <View style={{ flexDirection: 'row', marginHorizontal: 10 }} >
-                <Button style={styles.button} onPress={() => setMode('driving')}><MyIcon name="md-car" /> </Button>
-                <Button style={styles.button} onPress={() => setMode('walking')}><MyIcon name="md-walk" /></Button>
-                <Button style={styles.button} onPress={() => setMode('bicycling')}><MyIcon name="md-bicycle" /></Button>
-                <Button style={styles.button} onPress={() => setMode('transit')}><MyIcon name="md-train" /></Button>
+                <Button style={styles.button} onPress={() => { setMode('driving'); handleChange() }}><MyIcon name="md-car" /> </Button>
+                <Button style={styles.button} onPress={() => { setMode('walking'); handleChange() }}><MyIcon name="md-walk" /></Button>
+                <Button style={styles.button} onPress={() => { setMode('bicycling'); handleChange() }}><MyIcon name="md-bicycle" /></Button>
+                <Button style={styles.button} onPress={() => { setMode('transit'); handleChange() }}><MyIcon name="md-train" /></Button>
             </View>
         </View >
     );
