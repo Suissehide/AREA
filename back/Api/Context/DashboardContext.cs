a9ddf5ae3bcb00514070dbb2c005d549070432e0
Justena <justine.duhieu@epitech.eu>
2020-03-03 00:56:13 +0100

    Create new solution object and move the back into Api folder to create UnitTest in the same solution

---
+++ back/Api/Context/DashboardContext.cs
@@ -0,0 +1,11 @@
+﻿﻿using Microsoft.EntityFrameworkCore;
+
+namespace DashboardApi.Context
+{
+    public class DashboardContext : DbContext
+    {
+        public DashboardContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
