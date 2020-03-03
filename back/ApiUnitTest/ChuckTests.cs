4c085f34b41f07f2a0b536ffb1219fb7569b530a
Justena <justine.duhieu@epitech.eu>
2020-03-03 02:04:04 +0100

    Add the first test for Chuck Controller

---
+++ back/ApiUnitTest/ChuckTests.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using back.Controllers;
+using back.Models.ChuckApi;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace BackUnitTest
+{
+    public class ChuckTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public ChuckTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        public Dictionary<string, string> InitParam()
+        {
+            Dictionary<string, string> param = new Dictionary<string, string>();
+            param.Add("OneWord", "life");
+            param.Add("FrenchWord", "bonjour");
+            param.Add("NoWord", "");
+            param.Add("NullWord", null);
+            param.Add("MultipleWord", "life");
+            return param;
+        }
+        
+        [Fact]
+        public async Task OneWord()
+        {
+            // Arrange
+            
+            var logger = new Logger<ChuckController>(new LoggerFactory());
+            var param = InitParam()["OneWord"];
+            var chuckController = new ChuckController(logger);
+
+            // Act
+            var result = await chuckController.ChuckByTheme(param);
+
+            // Assert
+            var okResult = result as OkObjectResult;
+            var myChuckObject = okResult.Value as ChuckModel;
+            Assert.IsType<int>(myChuckObject.Total);
+            Assert.IsType<List<ChuckJokeModel>>(myChuckObject.Result);
+            Assert.IsType<string>(myChuckObject.Result[0].Value);
+        }
+    }
+}
